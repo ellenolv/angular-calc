e02697042ca7d65bf595c6233496fa3aa412204f
ellenolv <ellendeoliverapereira@gmail.com>
2023-03-26 15:39:09 -0300

    Versão final

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,11 +9,20 @@ export class CalcComponent {
  //Atributos da classe/componente
   num1 : number = 0;
   num2 : number = 0;
-  soma : number = 0;
+  result : number = 0;
 
   //para que seja uma variavel voce precisa declarar dentro do metodo
   somar(){
-    this.soma = this.num1+this.num2;//o this é usado caso queira usar atributos
+    this.result = this.num1+this.num2;//o this é usado caso queira usar atributos
     //caso eu decalre a variavel dentro do metodo, eu não preciso usar o this, ele só é util caso voce queira pegar do atributo(diferente de java)
+  }
+  subtrair(){
+    this.result = this.num1-this.num2;
+  }
+  dividir(){
+    this.result = this.num1/this.num2;
+  }
+  multiplicar(){
+    this.result = this.num1*this.num2;
   }
 }
